8c2b5588ef2c96410c4b3b8933219479f4886c23
Will Martin <wcmartin01@gmail.com>
2021-12-30 21:16:42 -0600

    2021 day 13

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from collections import Counter
+import copy
 
 def read_file(filename):
     inputs = []
@@ -12,9 +13,59 @@ def test():
     assert part2(inputs_test) == None
 
 def part1(inputs):
-    return None
+    inputs = copy.deepcopy(inputs)
+    instructions = []
+    for line in inputs:
+        line_str = line[0]
+        if line_str.startswith("fold"):
+            instructions.append(line_str.split()[-1].split("="))
+
+    del inputs[-len(instructions):]
+    inputs = [[int(x), int(y)] for x, y in inputs]
+
+    axis, val = instructions[0]
+    for dot in inputs:
+        if axis == "x":
+            if int(val) < dot[0]:
+                dot[0] -= (dot[0] - int(val))*2
+        if axis == "y":
+            if int(val) < dot[1]:
+                dot[1] -= (dot[1] - int(val))*2
+
+    return len([dot for i, dot in enumerate(inputs) if dot not in inputs[:i]])
 
 def part2(inputs):
+    inputs = copy.deepcopy(inputs)
+    instructions = []
+    for line in inputs:
+        line_str = line[0]
+        if line_str.startswith("fold"):
+            instructions.append(line_str.split()[-1].split("="))
+
+    del inputs[-len(instructions):]
+    inputs = [[int(x), int(y)] for x, y in inputs]
+
+    for instruction in instructions:
+        axis, val = instruction
+        for dot in inputs:
+            if axis == "x":
+                if int(val) < dot[0]:
+                    dot[0] -= (dot[0] - int(val))*2
+            if axis == "y":
+                if int(val) < dot[1]:
+                    dot[1] -= (dot[1] - int(val))*2
+
+    unique_dots = [dot for i, dot in enumerate(inputs) if dot not in inputs[:i]]
+    unique_dots.sort()
+
+    for c in range(min([y for x,y in unique_dots]), max([y for x,y in unique_dots])+1):
+        for r in range(min([x for x,y in unique_dots]), max([x for x,y in unique_dots])+1):
+            if [r, c] in unique_dots:
+                print("x", end='')
+            else:
+                print(" ", end='')
+        print()
+
     return None
 
 
